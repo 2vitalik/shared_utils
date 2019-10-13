976d986547115f56285cad6b832bc9d80c3161c3
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-10-14 00:25:22 +0300

    text: Add function "remove_spaces" (to be used for multiline strings)

--- shared_utils/text/strip.py
+++ shared_utils/text/strip.py
@@ -1,5 +1,12 @@
+import re
+
 
 def strip(text):
     if not text:  # для случая None
         return ''
     return text.strip()
+
+
+def remove_spaces(text):
+    """To be used in multiline strings."""
+    return re.sub('\n +', '\n', text)
