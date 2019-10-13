5e30bda74cde34d90e1d2d7f2983064cbdace354
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-10-13 22:32:26 +0300

    log: Добавление `log_status` из `coda_changes`, и пара исправлений

--- shared_utils/debug/log.py
+++ shared_utils/debug/log.py
@@ -7,18 +7,22 @@ from shared_utils.io.io import append
 from shared_utils.io.path import ensure_parent_dir
 
 
-def log(filename, line, path=None):
+def log_status(cron_key, line):
+    log_day('status', f'<{cron_key}>  {line}')
+
+
+def log(filename, line, path=conf.logs_path):
     if path:
         filename = join(path, filename)
     ensure_parent_dir(filename)
     append(filename, f'{dt()}: {line}')
 
 
-def log_day(slug, value, path=None):
+def log_day(slug, value, path=conf.logs_path):
     log(f"{slug}/{dtf('Ym/Ymd')}.txt", value, path=path)
 
 
-def log_hour(slug, value, path=None):
+def log_hour(slug, value, path=conf.logs_path):
     log(f"{slug}/{dtf('Ym/dh')}.txt", value, path=path)
 
 
@@ -30,6 +34,7 @@ def log_exception(slug):
             except Exception as e:
                 log(f"exceptions/{slug}/{dtf('Ym/Ymd')}.txt",
                     traceback.format_exc(), path=conf.logs_path)
+                # todo: send email or/and telegram message !!
                 raise
         return wrapped
     return decorator
