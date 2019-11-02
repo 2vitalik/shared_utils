a68a284d123f0b7a1ba092ec97ab0040153c37c7
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2019-11-03 01:23:32 +0200

    slack: Return False if some error while sending message

--- shared_utils/api/slack/core.py
+++ shared_utils/api/slack/core.py
@@ -1,5 +1,4 @@
 import json
-import logging
 
 import requests
 
@@ -8,12 +7,13 @@ from shared_utils.conf import conf
 
 def post_to_slack(chat, message, emoji=None):
     if conf.slack_disabled:
-        return
+        return False
     url = conf.slack_hooks.get(chat)
     if not url:
-        return
+        return False
     if emoji:
         message = f':{emoji}: {message}'
     payload = {"text": message}
     headers = {'content-type': 'application/json'}
     requests.post(url, data=json.dumps(payload), headers=headers)
+    return True
