ca9ffaf595e8699a9965fc3607526a0a57662018
Vitaliy Lyapota <vitaliy.lyapota@anvileight.com>
2020-01-17 01:13:53 +0200

    json: New function `json_dump_changed` (save json only if changed)

--- shared_utils/io/json.py
+++ shared_utils/io/json.py
@@ -14,3 +14,11 @@ def json_dump(filename, data):
 def json_load(filename):
     with open(filename, encoding='utf-8') as f:
         return json.load(f)
+
+
+@encoded_filename
+def json_dump_changed(filename, data):
+    new_data = json.dumps(data, indent=4, ensure_ascii=False)
+    old_data = json_load(filename) if exists(filename) else None
+    if old_data != new_data:
+        json_dump(filename, data)
